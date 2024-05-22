8b0bf6bad9a1e6ebdb62f851a4303edeed626859
jcradavelli <43486489+jcradavelli@users.noreply.github.com>
2024-05-22 19:06:43 -0300

    kicad official lib forked

--- scripts/FAB_generator/template_defs.py
+++
@@ -1,4 +0,0 @@
-# Template de documentacao
-gerber_layers_files = "F.Cu,B.Cu,F.Paste,B.Paste,F.Mask,B.Mask,User.Comments,F.SilkS,B.SilkS" # cada camada gera um arquivo
-gerber_layers_common = "Edge.Cuts" # aparece em todos os arquivos
-
